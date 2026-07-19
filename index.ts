e96df715b2d959a9197368444b31da9d562ef491
Nishat Yeasmin <nishatyasminnisha@gmail.com>
2026-07-19 22:46:39 +0600

    product CRUD operation completed

--- index.ts
+++ index.ts
@@ -83,6 +83,326 @@ const User = mongoose.model(
   userSchema
 );
 
+const verifyToken =
+async(
+req:AuthRequest,
+res:express.Response,
+next:express.NextFunction
+)=>{
+
+
+try{
+
+
+const token =
+req.cookies.token;
+
+
+if(!token){
+
+return res.status(401).json({
+message:"Unauthorized"
+});
+
+}
+
+
+const decoded:any =
+jwt.verify(
+token,
+process.env.JWT_SECRET as string
+);
+
+
+
+req.user = decoded;
+
+
+next();
+
+
+
+}
+catch(error){
+
+return res.status(401).json({
+message:"Invalid token"
+});
+
+
+}
+
+
+}
+//product schema
+
+const productSchema = new mongoose.Schema(
+{
+ title:{
+  type:String,
+  required:true
+ },
+
+ shortDescription:{
+  type:String,
+  required:true
+ },
+
+ description:{
+  type:String,
+  required:true
+ },
+
+ price:{
+  type:Number,
+  required:true
+ },
+
+ image:{
+  type:String,
+  default:""
+ },
+
+ seller:{
+  type:mongoose.Schema.Types.ObjectId,
+  ref:"User",
+  required:true
+ },
+  createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+},
+{
+ timestamps:true
+}
+
+);
+
+
+const Product = mongoose.model(
+"Product",
+productSchema
+);
+
+app.post("/api/products",verifyToken, async (req: AuthRequest, res) => {
+
+  try {
+
+    const {
+      title,
+      shortDescription,
+      description,
+      price,
+      image
+    } = req.body;
+
+    const product = await Product.create({
+      title,
+      shortDescription,
+      description,
+      price,
+      image,
+      seller: req.user?.id,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "Product added successfully",
+      product,
+    });
+
+  } catch (error) {
+ console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to add product",
+      error,
+    });
+
+  }
+
+});
+
+app.get("/api/products", async (req, res) => {
+
+  const products = await Product.find().sort({
+    createdAt: -1,
+  });
+
+  res.json(products);
+
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.json(product);
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(500).json({
+      message: "Failed to load product",
+    });
+
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const {
+      title,
+      shortDescription,
+      description,
+      price,
+      image,
+    } = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      id,
+      {
+        title,
+        shortDescription,
+        description,
+        price,
+        image,
+      },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Product updated successfully",
+      product: updatedProduct,
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to update product",
+    });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({
+        message: "Product not found",
+      });
+    }
+
+    res.json({
+      success: true,
+      message: "Product deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to delete product",
+    });
+  }
+});
+
+
+app.get(
+"/api/products/manage",
+verifyToken,
+async(req:AuthRequest,res)=>{
+
+
+const products =
+await Product.find({
+seller:req.user?.id
+});
+
+
+
+res.json(products);
+
+
+
+});
+
+app.delete(
+"/api/products/:id",
+verifyToken,
+async(req:AuthRequest,res)=>{
+
+
+await Product.findOneAndDelete({
+
+_id:req.params.id,
+
+seller:req.user?.id
+
+});
+
+
+res.json({
+
+message:"Deleted"
+
+});
+
+
+});
+
+app.get("/api/dashboard/stats", async (req, res) => {
+  try {
+    const totalUsers = await User.countDocuments();
+    const totalSellers = await User.countDocuments({
+      role: "seller",
+    });
+
+    const totalCustomers = await User.countDocuments({
+      role: "customer",
+    });
+
+    res.json({
+      totalUsers,
+      totalSellers,
+      totalCustomers,
+      totalProducts: 0,
+      activeOrders: 0,
+      revenue: 0,
+    });
+  } catch {
+    res.status(500).json({
+      message: "Failed",
+    });
+  }
+});
+
 
 app.get("/", (req, res)=>{
     res.send("Fresh Basket Server Running");
@@ -332,9 +652,6 @@ res.json({
 user
 
 });
-
-
-
 }
 
 catch(error){
